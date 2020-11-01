127cac8b38acc56b7df875011b04b9fdcd2257c4
Sam Clegg <sbc@chromium.org>
2020-10-31 19:48:14 -0700

    Implement some more of the wasi API
    In order to test our WASI API implementation I've added
    `wasi-test-suite` as submodule.  This submodule is only
    needed for testing.

--- tests/test_other.py
+++ tests/test_other.py
@@ -9872,3 +9872,28 @@ exec "$@"
     self.assertNotIn('profile', result)
     self.assertIn('Hello from main!', result)
     self.assertIn('Hello from lib!', result)
+
+  def run_wasi_test_suite_test(self, name):
+    self.node_args.append('--experimental-wasm-bigint')
+    wasm = path_from_root('tests', 'third_party', 'wasi-test-suite', name + '.wasm')
+    if not os.path.exists(wasm):
+      self.fail('wasi-test-suite not found; run `git submodule update --init`')
+    self.run_process([EMCC, '-Wno-experimental', '--post-link', '-g', '-sPURE_WASI', '-lnodefs.js', '-lnoderawfs.js', wasm, '-o', name + '.js'])
+
+    with open(path_from_root('tests', 'third_party', 'wasi-test-suite', name + '.json')) as f:
+      config = json.load(f)
+    exit_code = config.get('exitCode', 0)
+    args = config.get('args', [])
+    output = self.run_js(name + '.js', args=args, assert_returncode=exit_code)
+    if 'stdout' in config:
+      self.assertContained(config['stdout'], output)
+
+  def test_wasi_std_io_stdout(self):
+    self.node_args.append('--experimental-wasm-bigint')
+    self.run_wasi_test_suite_test('std_io_stdout')
+
+  def test_wasi_wasi_fd_write_file(self):
+    self.node_args.append('--experimental-wasm-bigint')
+    self.run_wasi_test_suite_test('wasi_fd_write_file')
+    with open('new_file') as f:
+      self.assertEqual(f.read(), 'new_file')
